91d0ef05f82d9eaa02ba539348df7eb728f0f43e
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-12 14:19:41 +0530

    Fixes PY-D0002

--- src/client.py
+++ src/client.py
@@ -2,5 +2,9 @@ from discord import Client
 
 
 class GitHubClient(Client):
+    """
+    Client for GitHub related interactions
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
