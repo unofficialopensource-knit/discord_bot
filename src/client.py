7667870312ebd6ac8c07fa7d84f854511dc656b5
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-12 16:28:44 +0530

    Fixes FLK-D200

--- src/client.py
+++ src/client.py
@@ -2,9 +2,7 @@ from discord import Client
 
 
 class GitHubClient(Client):
-    """
-    Client for GitHub related interactions
-    """
+    """Client for GitHub related interactions"""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
