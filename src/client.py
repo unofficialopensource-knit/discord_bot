11170ecddaf73bfa91ce481a604553df2039235c
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-12 16:59:39 +0530

    Removes on_ready handler

--- src/client.py
+++ src/client.py
@@ -1,14 +1,8 @@
-import logging
-
 from discord import Client
 
 
 class GitHubClient(Client):
     """Client for GitHub related interactions"""
-
-    async def on_ready(self):
-        logging.warn(f"Logged in as {self.user}")
-        print(f"Logged in as {self.user}")
 
     async def on_message(self, message):
         if message.author == self.user:
