a22e5c0894ceb497db10f31517b52b426c50d571
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-12 23:01:49 +0530

    Updates entrypoint script

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
 
-if [[ $DEPLOYMENT_PLATFORM == "heroku" ]]; then
-    if [[ $PROCESS_TYPE == "web" ]]; then
-        gunicorn --config "python:config.gunicorn"
-    fi
-elif [[ $DEPLOYMENT_PLATFORM == "local" ]]; then
-    if [[ $PROCESS_TYPE == "web" ]]; then
-        gunicorn --config "python:config.gunicorn"
-    fi
-fi
+gunicorn --config "python:config.wsgi"
