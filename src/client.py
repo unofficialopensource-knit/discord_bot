1bc748e6904b6d11b1963a1318921eaca2461834
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-12 16:49:54 +0530

    Adds a test command

--- src/client.py
+++ src/client.py
@@ -1,8 +1,18 @@
+import logging
+
 from discord import Client
 
 
 class GitHubClient(Client):
     """Client for GitHub related interactions"""
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    async def on_ready(self):
+        logging.warn(f"Logged in as {self.user}")
+        print(f"Logged in as {self.user}")
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith("$test"):
+            await message.channel.send("Hello, world!")
