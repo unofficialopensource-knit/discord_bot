ca28c9f584112fb3ab490636b375a9543bd7ac4a
Mayank Pathak <onlinejudge95@gmail.com>
2021-06-12 17:07:16 +0530

    Fixes PY-D0003

--- src/client.py
+++ src/client.py
@@ -5,6 +5,10 @@ class GitHubClient(Client):
     """Client for GitHub related interactions"""
 
     async def on_message(self, message):
+        """
+        Event handler for on_message event.
+        This is triggered whenever a message is sent ojn any channel in the server.
+        """
         if message.author == self.user:
             return
 
